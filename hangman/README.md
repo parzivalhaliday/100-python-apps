0c656416d6cd29a57b3218e35b35c65017401e65
parzivalhaliday <55963388+parzivalhaliday@users.noreply.github.com>
2023-04-05 07:26:09 +0300

    Add files via upload

---
+++ hangman/README.md
@@ -0,0 +1,26 @@
+# Hangman Game
+This is a simple implementation of the classic Hangman game in Python. In this game, the computer generates a random word, and the player needs to guess the word by suggesting letters one at a time. The player is allowed a maximum number of wrong guesses, and for each wrong guess, the computer draws a part of the "hanging man." If the player cannot guess the word within the allowed number of wrong guesses, the game is over, and the player loses.
+
+## Requirements
+This game requires the `random_words` library to be installed. You can install it using the following command:
+
+
+```pip install random_words```
+
+## How to Play
+To play the game, simply run the `hangman.py` file using Python. The game will generate a random word, and you will see a series of underscores representing the letters in the word. You need to guess the letters one at a time by typing them into the console. If the letter is in the word, it will be revealed in the correct position. If it is not in the word, the computer will draw a part of the hanging man.
+
+You are allowed a maximum number of wrong guesses, and if you cannot guess the word within the allowed number of wrong guesses, the game is over, and you lose. If you correctly guess all the letters in the word before the maximum number of wrong guesses, you win.
+
+## Features
+This implementation of the Hangman game includes the following features:
+
+* The game generates a random word using the `random_words` library.
+* The word is represented by a series of underscores.
+* The player can guess letters one at a time.
+* The game keeps track of the number of wrong guesses and draws the hanging man accordingly.
+* The game ends when the player correctly guesses all the letters or reaches the maximum number of wrong guesses.
+*The player is allowed to use a joker letter once if they have made five wrong guesses and have not used the joker yet.
+
+## Acknowledgments
+This Hangman game is based on the tutorial by Tech with Tim on YouTube. The ASCII art of the hanging man was taken from here.
---
+++ hangman/hangmangame.py
@@ -0,0 +1,144 @@
+import random
+from random_words import RandomWords
+
+rw = RandomWords()
+
+# Welcome message and instruction
+print("Welcome to Hangman game!")
+print("You need to guess a word chosen by the computer.")
+
+# Generate a random word using the RandomWords library
+word = rw.random_word()
+
+# Convert the word to a set of lowercase letters
+word_letters = set(word.lower())
+
+# Create a set of all letters in the alphabet
+alphabet = set("abcdefghijklmnopqrstuvwxyz")
+
+# Create an empty set to store used letters
+used_letters = set()
+
+# Initialize the number of wrong guesses and maximum wrong guesses allowed
+wrong_guesses = 0
+max_wrong_guesses = 6
+
+# Ask the user if they want to continue playing after the game is over
+continue_guess = "y"
+
+# Flag to check if the joker letter has been used
+joker_used = False
+
+# ASCII art of the hanging man for each stage of the game
+hangman_stages = ['''
+    --------
+    |      |
+    |      
+    |      
+    |      
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |      
+    |      
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |      |
+    |      
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |     /|
+    |      
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |     /|\\
+    |      
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |     /|\\
+    |     / 
+    |     
+    -
+''', '''
+    --------
+    |      |
+    |      O
+    |     /|\\
+    |     / \\
+    |     
+    -
+''']
+
+# Game loop
+while continue_guess == "y":
+    # Print the current stage of the hanging man
+    print(hangman_stages[wrong_guesses])
+
+    # Create a list of letters and underscores to represent the word
+    word_list = [letter if letter in used_letters else "_" for letter in word]
+    print(" ".join(word_list))
+
+    # Ask the user to guess a letter
+    guess = input("Guess a letter: ").lower()
+
+    # Check if the guessed letter is in the alphabet and has not been used before
+    if guess in alphabet - used_letters:
+        # Add the guessed letter to the used letters set
+        used_letters.add(guess)
+        # Check if the guessed letter is in the word
+        if guess in word_letters:
+            # Remove the guessed letter from the set of word letters
+            word_letters.remove(guess)
+        else:
+            # Increment the number of wrong guesses and print a message
+            wrong_guesses += 1
+            print("Wrong guess.")
+            # Check if the maximum number of wrong guesses has been reached
+            if wrong_guesses == max_wrong_guesses:
+                # If so, print the final stage of the hanging man and the correct word
+                print(hangman_stages[wrong_guesses])
+                print("Game over. The word was", word)
+                break
+    # Check if the guessed letter has been used before
+    elif guess in used_letters:
+        print("You have already used that letter. Please try again.")
+    # Check if the guessed character is not in the alphabet
+    else:
+        print("Invalid character. Please try again.")
+
+    # Check if all the letters in the word have been guessed
+    if not word_letters:
+        print("Congratulations! You have guessed the word", word)
+        break
+    #If the player has made 5 wrong guesses and hasn't used the joker yet, offer the option to use it
+    if wrong_guesses == 5 and not joker_used:
+        continue_guess = input("Do you want to use your joker letter? (y/n): ")
+        if continue_guess.lower() == "y":
+            # Select a random letter from the remaining letters in the word
+            joker_letter = random.choice(list(word_letters))
+            used_letters.add(joker_letter)
+            word_letters.remove(joker_letter)
+            joker_used = True
+        else:
+            continue_guess = "n"
+            
+print("Thanks for playing Hangman game!")
